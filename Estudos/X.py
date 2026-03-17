1ba145e61db089694e00b4960167a6218644c277
victor4500 <sla133278@gmail.com>
2026-03-17 15:11:41 -0300

    conta de multiplicação fixes #2

---
+++ Estudos/X.py
@@ -0,0 +1,7 @@
+valor1 = int (input("digite o primiero valor: "))
+
+valor2 = int (input("digite quantas vezes vc quer multiplicar: "))
+
+mult =(valor1 * valor2)
+
+print (f'resultado da multiplicação é: {mult}')
