f47c08ae3820a3567610953492d36a8a8d482197
victor4500 <sla133278@gmail.com>
2026-03-18 16:34:01 -0300

    adicionando porcentagem

---
+++ ContasBasicas/porcentagem.py
@@ -0,0 +1,9 @@
+valor1 = float (input("quantos % vc quer saber: "))
+
+valor2 = float (input("valor do numero: "))
+
+calculo = ((valor1 * valor2 ) / 100 )
+
+calculo2 = 100 / calculo
+
+print (f" %{valor1} de {valor2} e %{calculo2}")
